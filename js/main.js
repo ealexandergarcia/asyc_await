9fcdec679d67867d58c4e1e703cd28977fda770e
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-05 08:17:09 -0500

    feat: :sparkles: consumo y validacion de comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import { getAllAlbums,addAlbum } from "./module/album.js";
 import { getUser } from "./module/users.js";
-import { getAllPosts, addPost} from "./module/posts.js";
+import { getAllPosts, addPost, getPost} from "./module/posts.js";
+import { getAllComments, addComments} from "./module/comments.js";
 
-console.table(await addPost({userId:10, title:"Prueba", body:"Prueba de contenido"}))
+
+console.table(await addComments({postId:100, name:"Prueba",email:"prueba@gmail.com", body:"Prueba de contenido"}))
+// console.table(await getPost({postId:1}));
+// console.table(await addPost({userId:10, title:"Prueba", body:"Prueba de contenido"}))
 // console.table(await getAllPosts())
 // console.log(await getUser({userId: 1}));
 // console.table(await addAlbum({userId:1, title:"Prueba"}));
---
+++ js/module/comments.js
@@ -0,0 +1,27 @@
+import { getPost } from "./posts.js";
+export const getAllComments= async () => {
+    let res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    let data = await res.json();
+    return data;
+};
+
+const validateAddComments = async ({ postId, name,email, body }) => {
+    if (typeof name !== "string" || name === undefined) return { status: 406, message: ` The data ${name} is not arriving or does not comply with the requiered format` }
+    if (typeof email !== "string" || email === undefined) return { status: 406, message: ` The data ${email} is not arriving or does not comply with the requiered format` }
+    if (typeof body !== "string" || body === undefined) return { status: 406, message: ` The data ${body} is not arriving or does not comply with the requiered format` }
+    let user = await getPost({ postId })
+    if (user.status == 204) return { status: 200, message: `Post does not exist` }
+}
+
+export const addComments = async (arg) => {
+    let val = await validateAddComments(arg);
+    if (val) return val;
+    let config = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arg)
+    };
+    let res = await fetch("https://jsonplaceholder.typicode.com/comments", config);
+    let data = await res.json();
+    return data;
+};
