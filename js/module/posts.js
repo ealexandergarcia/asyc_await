0962af369af7a7b2fcb30606c40a76188017cc6d
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-06 18:43:38 -0500

    feat: ✨ Funcion de actualizar y eliminar de posts

--- js/module/posts.js
+++ js/module/posts.js
@@ -37,3 +37,39 @@ export const addPost = async (arg) => {
     let data = await res.json();
     return data;
 };
+
+export const updatePost = async (id, arg) => {
+    let val = await validateAddPost(arg);
+    if (val) return val;
+    let config = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arg)
+    };
+    let res = await fetch(`http://172.16.101.146:5800/posts/${id}`, config);
+    let data = await res.json();
+    return data;
+}
+
+export const patchPost = async (id, arg) => {
+    let config = {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arg)
+    };
+    let res = await fetch(`http://172.16.101.146:5800/posts/${id}`, config);
+    let data = await res.json();
+    return data;
+}
+
+export const deletePost = async (postId) => {
+    let config = {
+        method: "DELETE"
+    };
+    let res = await fetch(`http://172.16.101.146:5800/posts/${postId}`, config);
+    if (res.status === 404) return { status: 204, message: "the Post id does not exist or has an unaccepted format" }
+    let data = await res.json();
+    data.status = 202;
+    data.message = `The Post ${postId} was deleted from the database`
+    return data;
+}
