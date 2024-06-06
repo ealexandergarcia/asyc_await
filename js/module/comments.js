adbc28bc224d0530c46722291ac3fd8178f9aeb7
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-06 18:34:05 -0500

    feat: ✨ Funcion de actualizar y eliminar de comments

--- js/module/comments.js
+++ js/module/comments.js
@@ -1,11 +1,11 @@
 import { getPost } from "./posts.js";
-export const getAllComments= async () => {
+export const getAllComments = async () => {
     let res = await fetch("http://172.16.101.146:5801/comments");
     let data = await res.json();
     return data;
 };
 
-const validateAddComments = async ({ postId, name,email, body }) => {
+const validateAddComments = async ({ postId, name, email, body }) => {
     if (typeof name !== "string" || name === undefined) return { status: 406, message: ` The data ${name} is not arriving or does not comply with the requiered format` }
     if (typeof email !== "string" || email === undefined) return { status: 406, message: ` The data ${email} is not arriving or does not comply with the requiered format` }
     if (typeof body !== "string" || body === undefined) return { status: 406, message: ` The data ${body} is not arriving or does not comply with the requiered format` }
@@ -25,3 +25,39 @@ export const addComments = async (arg) => {
     let data = await res.json();
     return data;
 };
+
+export const updateComments = async (id, arg) => {
+    let val = await validateAddComments(arg);
+    if (val) return val;
+    let config = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arg)
+    };
+    let res = await fetch(`http://172.16.101.146:5801/comments/${id}`, config);
+    let data = await res.json();
+    return data;
+}
+
+export const patchComments = async (id, arg) => {
+    let config = {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arg)
+    };
+    let res = await fetch(`http://172.16.101.146:5801/comments/${id}`, config);
+    let data = await res.json();
+    return data;
+}
+
+export const deleteComments  = async(commentsId)=>{
+    let config = {  
+      method: "DELETE"
+    };
+    let res = await fetch(`http://172.16.101.146:5801/comments/${commentsId}`, config);
+    if(res.status ===404) return {status: 204, message:"the comment id does not exist or has an unaccepted format"}
+    let data = await res.json();
+    data.status = 202;
+    data.message =`The comment ${commentsId} was deleted from the database`
+    return data;
+  }
