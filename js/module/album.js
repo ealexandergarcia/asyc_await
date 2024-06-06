328b5a4c8a10d0e6472294092d959ba115b6a080
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-06 07:18:04 -0500

    chore: :hammer: cambio de endpoint del api en album

--- js/module/album.js
+++ js/module/album.js
@@ -1,13 +1,13 @@
 import { getUser } from "./users.js";
 
 export const getAllAlbums = async () => {
-  let res = await fetch("https://jsonplaceholder.typicode.com/albums");
+  let res = await fetch("http://172.16.101.146:5802/albums");
   let data = await res.json();
   return data;
 };
 
 const validateAddAlbum = async ({ userId, title }) =>{
-  if (typeof userId != "number" || userId === undefined) return { status: 406, message: ` The data ${userId} is not arriving or does not comply with the requiered format` }
+  if (typeof userId != "string" || userId === undefined) return { status: 406, message: ` The data ${userId} is not arriving or does not comply with the requiered format` }
   if (typeof title !== "string" || title === undefined) return { status: 406, message: ` The data ${title} is not arriving or does not comply with the requiered format` }
   let user = await getUser({userId})
   if(user.status == 204) return { status: 200, message: `Username does not exist` }
@@ -21,7 +21,7 @@ export const addAlbum = async (arg) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(arg)
   };
-  let res = await fetch("https://jsonplaceholder.typicode.com/albums", config);
+  let res = await fetch("http://172.16.101.146:5802/albums", config);
   let data = await res.json();
   return data;
 };
