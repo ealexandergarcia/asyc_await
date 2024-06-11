7df1a3f9db8484b0f29c59af8b26c8d49b8cb625
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-11 06:42:49 -0500

    feat: :sparkles: Validacion de datos para el patch de comments

--- js/module/comments.js
+++ js/module/comments.js
@@ -29,7 +29,7 @@ export const addComments = async (arg) => {
 export const updateComments = async (arg) => {
     let val = await validateAddComments(arg);
     if (val) return val;
-    let {id} = arg
+    let { id } = arg
     let config = {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
@@ -39,9 +39,47 @@ export const updateComments = async (arg) => {
     let data = await res.json();
     return data;
 }
+const validateUpdateComments = async ({ postId, name, email, body }) => {
+    let errors = {};
+
+    if (name !== undefined) {
+        if (typeof name !== "string") {
+            errors.name = `The data name is not arriving or does not comply with the required format`;
+        }
+    }
+
+    if (email !== undefined) {
+        if (typeof email !== "string") {
+            errors.email = `The data email is not arriving or does not comply with the required format`;
+        }
+    }
+
+    if (body !== undefined) {
+        if (typeof body !== "string") {
+            errors.body = `The data body is not arriving or does not comply with the required format`;
+        }
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return { status: 406, message: "Error en los datos", errors };
+    }
+
+    let user = await getPost({ postId })
+    if (user.status == 204) {
+        return { status: 200, message: `Post does not exist` }
+    }
+}
 
 export const patchComments = async (arg) => {
-    let {id} = arg;
+    let val = await validateUpdateComments(arg);
+    if (val) {
+        if (val.status === 200) {
+            return val;
+        } else {
+            return val;
+        }
+    }
+    let { id } = arg;
     let config = {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
@@ -52,14 +90,14 @@ export const patchComments = async (arg) => {
     return data;
 }
 
-export const deleteComments  = async(commentsId)=>{
-    let config = {  
-      method: "DELETE"
+export const deleteComments = async (commentsId) => {
+    let config = {
+        method: "DELETE"
     };
     let res = await fetch(`http://172.16.101.146:5801/comments/${commentsId}`, config);
-    if(res.status ===404) return {status: 204, message:"the comment id does not exist or has an unaccepted format"}
+    if (res.status === 404) return { status: 204, message: "the comment id does not exist or has an unaccepted format" }
     let data = await res.json();
     data.status = 202;
-    data.message =`The comment ${commentsId} was deleted from the database`
+    data.message = `The comment ${commentsId} was deleted from the database`
     return data;
-  }
+}
